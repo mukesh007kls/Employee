d29dd81b98ddb9a374b5cac2c77625ea7b70419c
mukesh007kls <mallikanti2000@gmail.com>
2023-06-13 18:58:23 +0530

    Created end points for jdbc methods

---
+++ src/main/java/com/spring/employee/controller/CRUDOpp.java
@@ -0,0 +1,70 @@
+package com.spring.employee.controller;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class CRUDOpp {
+    private Connection getConnection() throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/empData";
+        String userName = "root";
+        String password = "klsa2921";
+        return DriverManager.getConnection(url, userName, password);
+    }
+
+    private void executeConnection(String query) throws SQLException {
+        Connection connection = this.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.executeUpdate();
+        statement.close();
+        connection.close();
+    }
+
+    public void insertData(int employeeID, String name, String gender, String department, long salary) throws SQLException, IOException {
+        String query = new StringBuilder().append("insert into employee_data(employeeID,name,gender,department,salary) \n").append("values(?,?,?,?,?)").toString();
+        Connection connection = this.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query);
+
+        statement.setInt(1, employeeID);
+        statement.setString(2,name);
+        statement.setString(3, gender);
+        statement.setString(4, department);
+        statement.setLong(5, salary);
+
+        statement.executeUpdate();
+    }
+
+    public ResultSet getAllEmployeesData() throws SQLException {
+        String query = String.valueOf(new StringBuilder().append("select*from employee_data"));
+        Connection con = this.getConnection();
+        PreparedStatement statement = con.prepareStatement(query);
+        return statement.executeQuery();
+    }
+
+    public ResultSet getEmployeeDataByID(int employeeID) throws SQLException {
+        String query = String.format("select*from employee_data where employeeID=%s", employeeID);
+        Connection con = getConnection();
+        PreparedStatement statement = con.prepareStatement(query);
+        return statement.executeQuery();
+    }
+
+    public int updateEmployeeDataById(int employeeID,String field,Object value) throws IOException, SQLException {
+        String query;
+
+        if (field.equalsIgnoreCase("salary")){
+            String sa=value.toString();
+            query=String.format("update employee_data set salary=%s where employeeID=%s",Long.parseLong(sa),employeeID);
+        }else {
+            query=String.format("update employee_data set %s='%s' where employeeID=%s",field,value,employeeID);
+        }
+        Connection con=getConnection();
+        PreparedStatement statement=con.prepareStatement(query);
+        return statement.executeUpdate();
+    }
+
+    public int deleteEmployeeById(int employeeID) throws SQLException {
+        String query = String.format("delete from employee_data where employeeID=%s", employeeID);
+        Connection connection = this.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query);
+        return statement.executeUpdate();
+    }
+}
--- src/main/java/com/spring/employee/controller/EmployeeController.java
+++ src/main/java/com/spring/employee/controller/EmployeeController.java
@@ -3,33 +3,106 @@ package com.spring.employee.controller;
 import com.spring.employee.model.Employee;
 import com.spring.employee.service.IEmployeeServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.spring.employee.controller.CRUDOpp;
 
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-    @Autowired
-    IEmployeeServices iEmployeeServices;
-
-    @GetMapping(value = "/getAllEmployees")
-    public List<Employee> getAllEmployees(){
-        return iEmployeeServices.getAllEmployeeList();
+//    @Autowired
+//    IEmployeeServices iEmployeeServices;
+//
+//    @GetMapping(value = "/getAllEmployees")
+//    public List<Employee> getAllEmployees(){
+//        return iEmployeeServices.getAllEmployeeList();
+//    }
+//
+//    @PostMapping(value = "/addEmployees")
+//    public void addEmployees(@RequestBody List<Employee> employeeList){
+//        iEmployeeServices.addEmployee(employeeList);
+//    }
+//
+//    @PutMapping(value = "/updateEmployeeDepartment/{employyeID}/{department}")
+//    public void updateEmployeeDepartment(@PathVariable("employeeID") int employeeID,@PathVariable("department") String department){
+//        iEmployeeServices.updateEmployeeDepartment(employeeID);
+//    }
+//
+//    @DeleteMapping(value = "/deleteEmployeeByID/{employeeID}")
+//    public void deleteEmployeeByEmployeeId(@PathVariable("employeeID") int employeeID){
+//        iEmployeeServices.deleteEmployeeByEmployeeID(employeeID);
+//    }
+    CRUDOpp crudOpp=new CRUDOpp();
+    @PostMapping("/createEmp")
+    public ResponseEntity<String> createEmployee(@RequestBody Map<String,Object> record) {
+        try {
+            int employeeID = (int) record.get("employeeID");
+            String name = (String) record.get("name");
+            String gender = (String) record.get("gender");
+            String department = (String) record.get("department");
+            long salary = (int) record.get("salary");
+            crudOpp.insertData(employeeID, name, gender, department, salary);
+            return ResponseEntity.ok("Employee added successfully");
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @PostMapping(value = "/addEmployees")
-    public void addEmployees(@RequestBody List<Employee> employeeList){
-        iEmployeeServices.addEmployee(employeeList);
+    @DeleteMapping("/deleteEmp/{employeeID}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("employeeID") int employeeID){
+        try {
+            int deletedRows=crudOpp.deleteEmployeeById(employeeID);
+            if (deletedRows>0){
+                return ResponseEntity.ok("Employee deleted");
+            }else {
+                return ResponseEntity.ok("Employee not found");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @PutMapping(value = "/updateEmployeeDepartment/{employyeID}")
-    public void updateEmployeeDepartment(@PathVariable("employeeID") int employeeID,@PathVariable("department") String department){
-        iEmployeeServices.updateEmployeeDepartment(employeeID);
+    @GetMapping("/getEmployee/{employeeID}")
+    public ResponseEntity<Map<String,Object>> getEmployee(@PathVariable("employeeID") int employeeID)  {
+        try {
+            ResultSet resultSet = crudOpp.getEmployeeDataByID(employeeID);
+            if (resultSet.next()){
+                Map<String,Object> data=new HashMap<>();
+                data.put("employeeID",resultSet.getInt("employeeID"));
+                data.put("name",resultSet.getString("name"));
+                data.put("gender",resultSet.getString("gender"));
+                data.put("department",resultSet.getString("department"));
+                data.put("salary",resultSet.getLong("salary"));
+                return ResponseEntity.ok(data);
+            }else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    @DeleteMapping(value = "/deleteEmployeeByID/{employeeID}")
-    public void deleteEmployeeByEmployeeId(@PathVariable("employeeID") int employeeID){
-        iEmployeeServices.deleteEmployeeByEmployeeID(employeeID);
+    @PutMapping("/updateEmployee/{employeeID}")
+    public ResponseEntity<String> updateEmployee(@PathVariable("employeeID") int employeeID,@RequestBody Map<String,Object> data){
+        try{
+            int numberOfUpdatedRecords=0;
+            for (String key: data.keySet()) {
+                numberOfUpdatedRecords=crudOpp.updateEmployeeDataById(employeeID,key, data.get(key));
+            }
+            if (numberOfUpdatedRecords>0){
+                return ResponseEntity.ok("Data updated");
+            }else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (SQLException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
